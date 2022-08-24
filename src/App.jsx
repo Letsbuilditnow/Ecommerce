11ffaea877ac61a88bc0965619d2e84dd0eeeb6b
mmmmm1233212344 <abhaymilan8@gmail.com>
2022-08-24 14:35:41 +0530

    Finished Main Section Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Slider from "./components/Slider";
+import Announcement from "./components/Announcement";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 
 const App = () => {
-  return <Navbar></Navbar>
+  return(
+  <div>
+  <Announcement></Announcement>
+    <Navbar></Navbar>
+    <Slider></Slider>
+  </div>
+  ) 
 };
 
 export default App;
